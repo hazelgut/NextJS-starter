d5dfc3ccc752264100c2e08424498c7922998b16
hazelgut <lonk9599@gmail.com>
2026-04-24 20:05:39 +0300

    Initial commit

---
+++ index.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+function Header({title}) {
+
+    return (
+        <h1>
+            {title ? title : 'Default title'}
+        </h1>
+    );
+}
+
+function HomePage() {
+    const names = ['Alice', 'David', 'Ksusha'];
+    const {likes, setLikes} = useState(0);
+
+    function handleClick(e) {
+        setLikes(l=>l+1);
+    }
+
+    return (
+        <div>
+            <Header title="Develop. Preview. Ship."></Header>
+            <ul>
+                {names.map(name => (<li key={name}>{name}</li>))}
+            </ul>
+            <button onClick={handleClick}>Likes({likes})</button>
+        </div>
+    );
+}
